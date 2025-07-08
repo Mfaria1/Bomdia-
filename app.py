2619bec0232a34d24bec9b86eb0ed5c7f0d368ba
Mfaria1 <mariaeduardafarr@gmail.com>
2025-07-07 22:11:26 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("<h1 style='color:pink; text-align:center;'>Eu gosto de você 💖</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align:center; font-size:24px;'>Só queria que você soubesse isso :)</p>", unsafe_allow_html=True)
