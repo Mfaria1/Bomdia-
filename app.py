af25d3e05ccaea840cd2ba91d67f1be48541826e
Mfaria1 <mariaeduardafarr@gmail.com>
2025-07-07 22:18:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.markdown("<h1 style='color:pink; text-align:center;'>Eu gosto de você 💖</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align:center; font-size:24px;'>Só queria que você soubesse isso :)</p>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:pink; text-align:center;'>Good morning :) 💖</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align:center; font-size:24px;'>I hope you have a nice day at the office :)</p>", unsafe_allow_html=True)
+st.markdown("<p style='color:gray; text-align:center; font-size:16px;'>i hate you</p>", unsafe_allow_html=True)
